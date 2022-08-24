4dbfb4fec79f9257ccfa4564956dbe3471849f53
Muppet237 <42935807+Petmojen@users.noreply.github.com>
2022-08-24 10:42:38 +0200

    Legs choices added

--- Assets/Script/LegsTool.cs
+++ Assets/Script/LegsTool.cs
@@ -1,13 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LegsTool:MonoBehaviour {
 
+    public GameObject legsObject;
     public Sprite[] legChoices;
+
+    RawImage legsSprite;
     int choiceMade;
 
-    public void ChangeLegs() {
-        
+    void Start() {
+        legsSprite = legsObject.GetComponent<RawImage>();    
+    }
+
+    public void ChangeLegs(int choice) {
+        legsSprite.texture = legChoices[choice].texture;
     }
 }
